fded61093de944facb4e852d93f46346a931b080
KoutaChan <76502083+KoutaChan@users.noreply.github.com>
2021-06-07 14:58:04 +0900

    upload

---
+++ src/main/java/me/koutachan/specialitem/Event.kt
@@ -0,0 +1,31 @@
+package me.koutachan.specialitem
+
+import org.bukkit.ChatColor
+import org.bukkit.Material
+import org.bukkit.Sound
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.block.BlockPlaceEvent
+import org.bukkit.inventory.EquipmentSlot
+
+object Event : Listener {
+    @EventHandler
+    fun onPlayerInteractEvent(e: BlockPlaceEvent) {
+        if (e.block.type == Material.FARMLAND && e.player.isSneaking) {
+            if ((e.hand == EquipmentSlot.HAND && e.player.inventory.itemInMainHand.itemMeta?.displayName.equals("${ChatColor.GREEN}強化されたクワ")) || (e.hand == EquipmentSlot.OFF_HAND && e.player.inventory.itemInOffHand.itemMeta?.displayName.equals("${ChatColor.GREEN}強化されたクワ"))) {
+                for (x in -1..1) {
+                    for (z in -1..1) {
+                        val block = e.block.location.add(x.toDouble(), 0.0, z.toDouble())
+                        if(block.block.type == Material.GRASS_BLOCK || block.block.type == Material.DIRT) {
+                            val airCheck = e.block.location.add(x.toDouble(), 1.0 , z.toDouble())
+                            if (airCheck.block.type == Material.AIR || airCheck.block.type == Material.CAVE_AIR) {
+                                block.block.type = Material.FARMLAND
+                                e.player.playSound(e.player.location, Sound.ITEM_HOE_TILL, 1F, 1F)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/me/koutachan/specialitem/SpecialItem.kt
@@ -0,0 +1,34 @@
+package me.koutachan.specialitem
+
+import org.bukkit.Bukkit
+import org.bukkit.ChatColor
+import org.bukkit.Material
+import org.bukkit.NamespacedKey
+import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.ShapedRecipe
+import org.bukkit.plugin.java.JavaPlugin
+
+class SpecialItem : JavaPlugin() {
+    lateinit var plugin: JavaPlugin
+    override fun onEnable() {
+        plugin = this
+        val recipe1Item = ItemStack(Material.DIAMOND_HOE)
+        val recipe1Meta = recipe1Item.itemMeta
+        recipe1Meta?.setDisplayName("${ChatColor.GREEN}強化されたクワ")
+        recipe1Meta?.lore = mutableListOf("${ChatColor.YELLOW}シフトをして耕すことで3x3のエリアを耕せる","${ChatColor.RED}このアイテムをアンビルで使わないでください、使用不可能になります。")
+        recipe1Item.itemMeta = recipe1Meta
+
+        val recipe1 = ShapedRecipe(NamespacedKey(this,"diamond_hoe"), recipe1Item)
+        recipe1.shape("DD "," S "," S ")
+        recipe1.setIngredient('D', Material.DIAMOND_BLOCK)
+        recipe1.setIngredient('S', Material.STICK)
+        Bukkit.addRecipe(recipe1)
+
+        server.pluginManager.registerEvents(Event,this)
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
